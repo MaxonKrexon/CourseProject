dc2af1d5f5e0441c4584d51668ee0a01293a419d
MaxonKrexon <korotkovmaksim2012@ya.ru>
2022-12-06 01:48:00 +0600

    init commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FreshSight.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
+
+// dotnet aspnet-codegenerator identity -dc WEBAPPTASK4.Data.ApplicationDbContext --files "Account.Register;Account.Login"
