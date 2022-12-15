f661688b653151fad6fcacf179cbf2785aee4ff6
MaxonKrexon <korotkovmaksim2012@ya.ru>
2022-12-15 23:43:11 +0600

    Changes in data structure for Blog.Index

--- Controllers/BlogController.cs
+++ Controllers/BlogController.cs
@@ -26,7 +26,7 @@ public class BlogController : Controller
         _userManager = userManager;
         _db = db;
     }
-    public Dictionary<String, String[,]> Properties = new Dictionary<string, string[,]>();
+    public Dictionary<String, List<String[]>> Properties = new Dictionary<string, List<string[]>>();
     public Azure.Pageable<Azure.Storage.Blobs.Models.BlobItem>? pic { get; set; }
     static String connectionString = "DefaultEndpointsProtocol=https;AccountName=freshsightcloud;AccountKey=nnVOWYu0nVMx1pprfPeoktl2PdAsTdmW/iL8Zt/CfqrP3xugfFM72Kpi47/l46qrfhBCIMDMliQ++AStPFLjHw==;EndpointSuffix=core.windows.net";
 
@@ -50,7 +50,7 @@ public class BlogController : Controller
     {
         String containerName = "images";
         BlobContainerClient imagesCli = new BlobContainerClient(connectionString, containerName);
-        var ProfilePic = new String[1, 1];
+        Properties.Add("PicLink", new List<String[]>());
         pic = imagesCli.GetBlobs(prefix: $"{user.Id}_pic");
         String picName = String.Empty;
         if (pic.Count() != 0)
@@ -59,9 +59,8 @@ public class BlogController : Controller
         }
         else picName = "default";
 
-        ProfilePic[0, 0] = $"https://freshsightcloud.blob.core.windows.net/images/{picName}_pic";
-
-        Properties.Add("PicLink", ProfilePic);
+        String[] link = { $"https://freshsightcloud.blob.core.windows.net/images/{picName}_pic" };
+        Properties["PicLink"].Add(link);
     }
 
 
@@ -72,13 +71,11 @@ public class BlogController : Controller
         var userPosts = _db.Posts.Where(p => p.Author.Id == user.Id).ToList();
         if (userPosts.Count() > 0)
         {
+            Properties.Add("Posts", new List<String[]>());
             foreach (var userPost in userPosts)
             {
-                var postProps = new String[1, 3];
-                postProps[0, 0] = userPost.ID;
-                postProps[0, 1] = userPost.Topic;
-                postProps[0, 2] = userPost.Category;
-                Properties.Add("Posts", postProps);
+                String[] values = { userPost.ID, userPost.Category, userPost.Topic };
+                Properties["Posts"].Add(values);
             }
         }
     }
@@ -89,10 +86,20 @@ public class BlogController : Controller
         if (user.DateOfBirth != null)
         {
             var span = DateTime.Now.Subtract((DateTime)user.DateOfBirth);
-            var age = new String[1, 1];
-            age[0, 0] = (span.Days / 365).ToString();
-            Properties.Add("Age", age);
+            Properties.Add("Age", new List<String[]>());
+            String[] value = { (span.Days / 365).ToString() };
+            Properties["Age"].Add(value);
         }
+    }
+
+
+    [HttpPost]
+    public IActionResult ViewPost()
+    {
+        var postId = Request.Form["id"].ToString();
+        var post = _db.Posts.Where(p => p.ID == postId);
+        return View(post);
+
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
